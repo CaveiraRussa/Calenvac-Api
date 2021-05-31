b58a8ff524901fc0339214795cfb04c3a66823ef
Luiz Santiago <luiz.santiago@championx.com>
2021-05-31 20:24:46 -0300

    Edit connections

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,40 +1,50 @@
-"use strict"
+"use strict";
 
-const User = use("App/Models/User")
+const User = use("App/Models/User");
 
 /** @type {import('@adonisjs/framework/src/Hash')} */
-const Hash = use('Hash')
+const Hash = use("Hash");
 
 class UserController {
-  async create ({ request }) {
-    const data = request.only(["cellphone", "password","name", "age", "address", "gender", "profession"])
+  async create({ request }) {
+    const data = request.only([
+      "cellphone",
+      "password",
+      "name",
+      "age",
+      "address",
+      "gender",
+      "profession",
+    ]);
 
-    const user = await User.create(data)
+    const user = await User.create(data);
 
-    return user
+    return user;
   }
 
-  async verify ({ request }) {
-    const { cellphone } = request.all()
+  async verify({ request }) {
+    const { cellphone } = request.all();
 
-    const user = await User
-    .query()
-    .where('cellphone', cellphone)
-    .fetch()
+    const User = use("App/Models/User");
 
-    return user
+    return await User.findBy("cellphone", cellphone);
   }
 
-  async update ({ request }) {
-    const { cellphone, password } = request.all()
+  async profile({ request }) {
+    const { cellphone } = request.all();
 
-    const user = await User
-    .query()
-    .where('cellphone', cellphone)
-    .update({ password: await Hash.make(password) })
+    const user = await User.query().where("cellphone", cellphone).fetch();
 
-    return user
+    return user;
+  }
+
+  async update({ request }) {
+    const { cellphone, password } = request.all();
+
+    return await User.query()
+      .where("cellphone", cellphone)
+      .update({ password: await Hash.make(password) });
   }
 }
 
-module.exports = UserController
+module.exports = UserController;
--- start/routes.js
+++ start/routes.js
@@ -4,6 +4,7 @@ const Route = use('Route')
 
 Route.post('/users', 'UserController.create')
 Route.post('/users/verify', 'UserController.verify')
+Route.post('/users/profile', 'UserController.profile')
 Route.post('/users/change', 'UserController.update')
 
 Route.post('/sessions', 'SessionController.create')
